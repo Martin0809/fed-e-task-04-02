1c4125add4101e5ccf218278b7f5a3884e043518
Martin0809 <maning0809@gmail.com>
2020-11-18 15:19:53 +0800

    fix: update publicPath

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,16 @@ const { override, addWebpackAlias, addLessLoader } = require('customize-cra')
 
 module.exports = {
   webpack: override(
+    (config, env) => {
+      if (env === 'production') {
+        config.output = {
+          ...config.output,
+          publicPath: '/fed-e-task-04-02/',
+        }
+      }
+
+      return config
+    },
     addWebpackAlias({
       '@': path.resolve(__dirname, 'src'),
     }),
